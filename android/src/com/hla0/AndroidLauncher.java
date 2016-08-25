5e282fe651bc4bc14e39988932020cee143d98e8
Herman La <hla0@ucla.edu>
2016-08-25 10:21:07 -0700

    more ad stuff

--- android/src/com/hla0/AndroidLauncher.java
+++ android/src/com/hla0/AndroidLauncher.java
@@ -22,18 +22,8 @@ public class AndroidLauncher extends AndroidApplication {
 		// Create the layout
 		RelativeLayout layout = new RelativeLayout(this);
 
-		// Do the stuff that initialize() would do for you
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-
 		// Create the libgdx View
 		View gameView = initializeForView(new Swatch(), config);
-
-		// Create and setup the AdMob view
-		//AdView adView = new AdView(this, AdSize.BANNER, "xxxxxxxx"); // Put in your secret key here
-		//adView.loadAd(new AdRequest());
 
 		// Add the libgdx view
 		layout.addView(gameView);
